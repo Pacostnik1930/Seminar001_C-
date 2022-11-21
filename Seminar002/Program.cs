749fa0e21bd4238f1373f4c498515f075de92875
Петр < >
2022-11-21 21:19:11 +0300

    Seminar002_C#

--- Seminar002/Program.cs
+++ Seminar002/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2 * number2){
+ Console.WriteLine ("да");
+} 
+ else{
+    Console.WriteLine("Нет");
+}
